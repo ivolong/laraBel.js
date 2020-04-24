a22dce3b255ab580c242fa6eee2aec5aefebd299
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-24 22:14:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
-const server = express()
-const port = 5000
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-server.get('/', (req, res) => res.send('big yeet'))
-
-server.listen(port, () => console.log(`Server running`))
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
