291363658097214177f9179d86082225047d420b
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-24 22:28:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT
 
 app.get('/', function (request, response) {
-    response.sendFile(path.join(__dirname + '/index.html'))
+    response.sendFile(path.join('/index.html'))
 })
 
 app.listen(port, () => console.log(`Server listening to port ${port}`))
