b079aba56893ede4a30fa2e4e722770b5e15da2b
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-24 21:52:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('big yeet');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('big yeet');
 }).listen(8080);
