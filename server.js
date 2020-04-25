d85aaf7726a6480b5434e550e7e0b10c67220869
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-25 12:38:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require('express')
-const php_express = require('php-express')({
-    binPath: 'php'
-})
 const app = express()
 const path = require('path')
 const port = process.env.PORT
 
-app.set('views', './views');
-app.engine('php', php_express.engine);
-app.set('view engine', 'php');
-app.all(/.+\.php$/, php_express.router);
-
 app.get('/', function (request, response) {
-    response.sendFile(path.join(__dirname + '/index.php'))
+    response.sendFile(path.join(__dirname + '/index.html'))
 })
 
 app.listen(port, () => console.log(`Server listening to port ${port}`))
