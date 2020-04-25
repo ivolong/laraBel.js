9f9716a4beb10fd95e8f7b3a48e3053046beb2ff
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-25 12:33:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const php_express = require('php-express'){(
+    binPath: 'php'
+)}
 const app = express()
 const path = require('path')
 const port = process.env.PORT
+
+app.set('views', './views');
+app.engine('php', phpExpress.engine);
+app.set('view engine', 'php');
+app.all(/.+\.php$/, phpExpress.router);
 
 app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname + '/index.php'))
