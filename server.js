8cefc489db0f76057c9cd89893a39980f9218b19
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-27 16:08:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = process.env.PORT
-
-const controller = require(__dirname + '/controllers/controller.js')
-
-app.get('/', controller.staticController.index())
+const routes = require(__dirname + '/routes.js')
 
 app.listen(port, () => console.log(`Server listening to port ${port}`))
-
-module.exports = controller
