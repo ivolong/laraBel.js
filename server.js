348459deac8592d2f29f9bf34bb6c15a069888e4
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-27 16:19:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = process.env.PORT
-const routes = require(__dirname + '/routes.js')
+const routes = require(__dirname + "/routes.js")
 
-app.listen(port, () => console.log(`Server listening to port ${port}`))
+const server = require("http").createServer(app)
+
+server.listen(port, () => console.log(`Server listening to port ${port}`))
