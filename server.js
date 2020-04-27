f016d6fbd9af7016ce63fc9fddadbe34c5bb06a1
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-27 15:28:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const route = express.Router()
 const path = require('path')
 const port = process.env.PORT
 
-app.get('/', function (request, response) {
-    response.sendFile(path.join(__dirname + '/index.html'))
+// app.get('/', function (request, response) {
+//     response.sendFile(path.join(__dirname + '/index.html'))
+// })
+
+route.get('/', function (req, res) {
+  res.send('big yeet');
 })
 
 app.listen(port, () => console.log(`Server listening to port ${port}`))
