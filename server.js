ba2326ae454e22577cced21562373c6cd3050f32
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-27 15:45:16 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 const port = process.env.PORT
 
+const controller = require(__dirname + '/controllers/controller.js')
+
 app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname + '/index.html'))
 })
