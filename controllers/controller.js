838d3f9410b7c7b679834e6f7942f16a615bcc33
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-27 16:32:21 +0100

    Update and rename controller.js to controllers.js

--- controllers/controller.js
+++
@@ -1,7 +0,0 @@
-const controller = {
-    index: function (request, response) {
-        return response.send("big yeet")
-    }
-}
-
-module.exports = controller;
---
+++ controllers/controllers.js
@@ -0,0 +1,1 @@
+const static = require(__dirname + "/static.js")
