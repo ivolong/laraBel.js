57e1c7d144094a537391fb06ce957b918a1987f7
ivo <36451843+ivolong@users.noreply.github.com>
2020-04-28 02:01:46 +0100

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,7 +3,11 @@ const viewer = require(__dirname + "/../framework/viewer.js")
 const controller = {
     index: function (request, response) {
         return viewer.send(response, "index.html")
-    }
+    },
+    
+    // about: function (request, response) {
+    //    return viewer.send(response, "about.html")
+    // }
 }
 
 module.exports = controller
