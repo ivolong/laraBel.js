8a26b6e9efe07717c77c44e30f6056d01f719ac5
ivo <36451843+ivolong@users.noreply.github.com>
2020-05-06 03:36:52 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,5 @@
 const controller = require(__dirname + "/controllers/controller.js")
-// const nameController = require(__dirname + "/controllers/nameController.js")
 
 module.exports = function(app) {
     app.get('/', controller.index)
-    // app.get("/about", nameController.about)
 }
