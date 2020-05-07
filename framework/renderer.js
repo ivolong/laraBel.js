252779a4e352806c84cf720c97e735a17f3cc311
ivo <36451843+ivolong@users.noreply.github.com>
2020-05-07 08:12:36 +0100

    Create renderer.js

---
+++ framework/renderer.js
@@ -0,0 +1,40 @@
+function escapeCharacters (text) {
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    }
+
+    return text.replace(/[&<>"']/g, function (m) {
+        return map[m]
+    })
+}
+
+module.exports = {
+    this.html = null,
+    this.data = null,
+    
+    render: function (html, data) {
+        this.data = data
+        this.html = html
+        return this.html.variables()
+    },
+    
+    variables: function () {
+        for (property in this.data) {
+            this.html = this.html.replace(
+                '{{ ' + property + ' }}',
+                escapeCharacters(this.data[property])
+            )
+                
+            html = html.replace(
+                '{!! ' + property + ' !!}',
+                this.data[property]
+            )
+        }
+        
+        return this.html
+    },
+}
