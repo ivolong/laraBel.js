ea19b42256ce4190f6de738e56d2e65ea3cff700
ivo <36451843+ivolong@users.noreply.github.com>
2020-05-07 06:25:12 +0100

    Update viewer.js

--- framework/viewer.js
+++ framework/viewer.js
@@ -1,9 +1,20 @@
 const path = require("path")
 
 const viewer = {
-    send: function (response, file_uri) {
-        return response.sendFile(path.join(__dirname + "/../views/" + file_uri))
-    }
+    data: {},
+    view: null,
+    
+    file: function (file_uri) {
+        this.view = __dirname + "/../views/" + file_uri)
+    },
+    
+    with: function (data) {
+        this.data[data.key] = data.value 
+    },
+    
+    send: function (response) {
+        return response.sendFile(path.join(this.view))
+    },
 }
 
 module.exports = viewer
