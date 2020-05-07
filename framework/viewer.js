42e253df7d5c3ce1360c8bcff898afd00c48e698
ivo <36451843+ivolong@users.noreply.github.com>
2020-05-07 06:27:53 +0100

    Update viewer.js

--- framework/viewer.js
+++ framework/viewer.js
@@ -6,10 +6,12 @@ const viewer = {
     
     file: function (file_uri) {
         this.view = __dirname + "/../views/" + file_uri
+        return this
     },
     
     with: function (data) {
         this.data[data.key] = data.value 
+        return this
     },
     
     send: function (response) {
